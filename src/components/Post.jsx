da2b710957e652d2686b65bba52c4b2cf7b7b986
Lucas dos Santos <72577150+Lucas76512@users.noreply.github.com>
2023-02-26 19:29:48 -0300

    assets

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,8 +29,13 @@ export function Post({ author,publishedAt, content }) {
              setNewCommentText('')
          }
 
-         function handleNewCommentChange() {
-             setNewCommentText(event.target.value)
+         function handleNewCommentChange() { 
+            event.target.setCustomValidity('')
+            setNewCommentText(event.target.value)
+         }
+
+         function handleNewCommentInvalid() {
+           event.target.setCustomValidity('Esse campo é obrigatorio')
          }
 
          function deleteComment(commentToDelete) {
@@ -39,6 +44,8 @@ export function Post({ author,publishedAt, content }) {
             })   
             setComments(commentsWithoutDeletedOne);
           } 
+
+         const isNewCommentEmpty = newCommentText.length == 0
 
          return (
         <article className={styles.post}>
@@ -74,10 +81,12 @@ export function Post({ author,publishedAt, content }) {
                   placeholder='Deixe um comentario'
                   value={newCommentText}
                   onChange={handleNewCommentChange}
+                  onInvalid={handleNewCommentInvalid}
+                  required
                 />
 
                 <footer>
-                <button type='submit'>Publicar</button>
+                <button type='submit'disabled={isNewCommentEmpty}>Publicar</button>
                 </footer> 
             </form>
 
